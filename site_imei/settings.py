30a12f49a8da4d707de20a54b053bdffb5c49661
leohuangyy <leo.huangyy@qq.com>
2018-05-02 10:30:29 +0800

    wrote a readme.md and requirements.txt

--- site_imei/settings.py
+++ site_imei/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from decouple import config
+from decouple import config,Csv
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +26,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=False,cast=bool)
 
-ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
 
 
 # Application definition
@@ -126,7 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'),]
+STATIC_ROOT= [os.path.join(BASE_DIR,'static'),]
 
 INTERNAL_IPS = ('127.0.0.1',)
 #INTERNAL_IPS = ('Gateway')
